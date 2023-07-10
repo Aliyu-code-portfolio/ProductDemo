5632eb2e8192dc472886386ba0f008c5c12f2c42
Aliyu Abdullahi <aliyuabdullahi074@gmail.com>
2023-07-10 11:00:59 +0100

    Docker added

--- ProductSystem/Data/RepositoryContext.cs
+++ ProductSystem/Data/RepositoryContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using ProductSystem.Models;
 
 namespace ProductSystem.Data
@@ -7,7 +9,26 @@ namespace ProductSystem.Data
     {
         public RepositoryContext(DbContextOptions<RepositoryContext> options):base(options)
         {
-            
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if (databaseCreator != null)
+                {
+                    if (!databaseCreator.CanConnect())
+                    {
+                        databaseCreator.Create();
+                    }
+                    if (!databaseCreator.HasTables())
+                    {
+                        databaseCreator.CreateTables();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                 Console.WriteLine(ex.Message);
+            }
         }
         DbSet<Product> Products { get; set; }
     }
